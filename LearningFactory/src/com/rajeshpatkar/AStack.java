34c86e88b65e8c22e4b3e15f112e6e75fbb4f8ef
Rajesh Patkar <patkar@rajeshpatkar.com>
2016-12-10 20:41:15 +0530

    Stage1
    Added AStack Class

---
+++ LearningFactory/src/com/rajeshpatkar/AStack.java
@@ -0,0 +1,16 @@
+
+package com.rajeshpatkar;
+
+/**
+ *
+ * @author rajeshpatkar
+ */
+public class AStack {
+    public void push(int v){
+        System.out.println("push of AStack");
+    }
+    public int pop(){
+        System.out.println("pop of AStack");
+        return 0;
+    }
+}
--- LearningFactory/src/learningfactory/LearningFactory.java
+++ LearningFactory/src/learningfactory/LearningFactory.java
@@ -1,6 +1,8 @@
 
 package learningfactory;
 
+import com.rajeshpatkar.AStack;
+
 /**
  *
  * @author rajeshpatkar
@@ -12,6 +14,13 @@ public class LearningFactory {
      */
     public static void main(String[] args) {
         System.out.println("Learning Factory");
+        AStack s1 = new AStack();
+        AClient(s1);
+    }
+    
+    public static void AClient(AStack s){
+        s.push(10);
+        s.pop();
     }
     
 }
