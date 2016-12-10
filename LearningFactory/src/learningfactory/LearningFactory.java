1f22936a8d88a498c32c6b7a57610268bd098fea
Rajesh Patkar <patkar@rajeshpatkar.com>
2016-12-10 20:18:43 +0530

    Factory Hello World

---
+++ LearningFactory/src/learningfactory/LearningFactory.java
@@ -0,0 +1,17 @@
+
+package learningfactory;
+
+/**
+ *
+ * @author rajeshpatkar
+ */
+public class LearningFactory {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Learning Factory");
+    }
+    
+}
